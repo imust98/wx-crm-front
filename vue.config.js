fe0e195b5604c40798c2a7fb18fb5827156b18b4
lhl <lihaoliang2006@126.com>
2019-01-24 16:15:11 +0800

    ci: 修改vue.config.js配置

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,10 @@
-module.exports = {};
+module.exports = {
+  // devServer: {
+  //   overlay: {
+  //     warnings: true,
+  //     errors: true
+  //   }
+  // },
+  lintOnSave: process.env.NODE_ENV !== 'production',
+  productionSourceMap: process.env.NODE_ENV !== 'production'
+};
