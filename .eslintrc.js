3886916459369dfe2fea75d63c21b9b89c68859d
lhl <lihaoliang2006@126.com>
2019-01-25 14:38:14 +0800

    ci: 增加eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,19 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-alert": 2
+    "no-alert": 2,
+    "eqeqeq": 2,
+    "no-empty-function": 2,
+    "no-empty-pattern": 2,
+    "no-eq-null": 2,
+    "no-eval": 2,
+    "no-extend-native": 2,
+    "no-extra-bind": 2,
+    "no-fallthrough": 2,
+    "no-implicit-coercion": 1,
+    "no-lone-blocks": 2,
+    "no-loop-func": 2,
+    "no-magic-numbers": 2,
   },
   parserOptions: {
     parser: "babel-eslint"
