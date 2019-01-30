4afce16234a60819d97c40d27a7a64ab005dc77f
lhl <lihaoliang2006@126.com>
2019-01-30 15:24:24 +0800

    ci: 添加loader svg 文件

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,27 @@
-module.exports = {
-  devServer: {
-    // overlay: {
-    //   warnings: true,
-    //   errors: true
-    // }
-    proxy: 'http://localhost:8002'
-  },
-  lintOnSave: process.env.NODE_ENV !== 'production',
-  productionSourceMap: process.env.NODE_ENV !== 'production'
-};
+ const path = require('path');
+ module.exports = {
+   devServer: {
+     // overlay: {
+     //   warnings: true,
+     //   errors: true
+     // }
+     proxy: 'http://localhost:8002'
+
+   },
+   chainWebpack: config => {
+     config.module.rule('svg')
+       .exclude.add(path.resolve('./src/icons'))
+       .end();
+     config.module.rule('icons')
+       .test(/\.svg$/)
+       .include.add(path.resolve('./src/icons'))
+       .end()
+       .use('svg-sprite-loader')
+       .loader('svg-sprite-loader')
+       .options({
+         symbolId: 'icon-[name]'
+       })
+   },
+   lintOnSave: process.env.NODE_ENV !== 'production',
+   productionSourceMap: process.env.NODE_ENV !== 'production'
+ };
